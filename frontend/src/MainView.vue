0ce030018a3a730e682cbd63983bd22450baf8c1
KR9SIS <kristoferhelgi+github@protonmail.com>
2025-07-07 16:02:29 +0200

    Started working on the site header which decides the CSS to use
    So far I've set up the checkboxes with initial values and made it
    dynamically change the font size on the page

---
+++ frontend/src/MainView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+import SiteHeader from './components/SiteHeader.vue'
+</script>
+
+<template>
+  <SiteHeader />
+  <body>
+    <h1>Title</h1>
+  </body>
+</template>
+
+<style scoped>
+.header {
+  margin: '100px';
+}
+.options {
+  padding: '10px';
+}
+</style>
---
+++ frontend/src/components/SiteHeader.vue
@@ -0,0 +1,192 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+// const arrayRange = (start, stop, step) =>
+//   Array.from({ length: (stop - start) / step + 1 }, (value, index) => start + index * step)
+
+const cssAffectPage = ref(false)
+
+const fontSize = ref('12')
+const fontSizes = ref([
+  { value: '8' },
+  { value: '9' },
+  { value: '10' },
+  { value: '11' },
+  { value: '12' },
+  { value: '14' },
+  { value: '18' },
+  { value: '24' },
+  { value: '30' },
+  { value: '36' },
+  { value: '48' },
+  { value: '60' },
+  { value: '72' },
+  { value: '96' },
+  { value: 'Custom' },
+])
+watch(
+  fontSize,
+  (fontSize, oldFontSize) => {
+    if (cssAffectPage.value === true && fontSize !== oldFontSize) {
+      document.documentElement.style.setProperty('--user-font-size', `${fontSize}pt`)
+      console.log(!fontSizes.value.includes({ value: fontSize.toString() }))
+      console.log({ value: fontSize.toString() })
+      console.log(fontSizes.value)
+      if (!fontSizes.value.some((size) => size.value === fontSize.toString())) {
+        fontSizes.value.push({ value: fontSize.toString() })
+      }
+      // TODO: maybe add localStorage.setItem('fontSize', size.toString())
+    }
+  },
+  // { immediate: true },
+)
+// Text Size Settings //
+// const fontSize = ref(parseFloat(localStorage.getItem('fontSize') || '1'))
+//
+// watch(
+//   fontSize,
+//   (size) => {
+//     document.documentElement.style.setProperty('--user-font-size', `${size}rem`)
+//     localStorage.setItem('fontSize', size.toString())
+//   },
+//   { immediate: true },
+// )
+const fontFamily = ref('Lexend')
+const fontFamilies = ref([
+  {
+    text: 'Lexend',
+    value: 'Lexend',
+    url: 'https://fonts.googleapis.com/css2?family=Lexend:wght@400;700&display=swap',
+  },
+  {
+    text: 'Times New Roman',
+    value: 'Times New Roman',
+    url: 'https://fonts.googleapis.com/css2?family=Times+New+Roman:wght@400;700&display=swap',
+  },
+  {
+    text: 'Comic Sans MS',
+    value: 'Comic Sans MS',
+    url: 'https://fonts.googleapis.com/css2?family=Comic+Sans+MS:wght@400;700&display=swap',
+  },
+])
+// document.documentElement.style.setProperty('--user-font-family', fontFamily.value)
+//
+// // Load the font
+// function loadFont(fontName: string, url: string) {
+//   if (!url) return
+//
+//   const existing = document.querySelector(`link[data-font="${fontName}"]`)
+//   if (existing) return // Avoid duplicate loads
+//
+//   const link = document.createElement('link')
+//   link.rel = 'stylesheet'
+//   link.href = url
+//   link.setAttribute('data-font', fontName)
+//   document.head.appendChild(link)
+// }
+//
+const fontColor = ref('#000000')
+const fontColors = ref([
+  { text: 'Black', value: '#000000' },
+  { text: 'Grey', value: '#808080' },
+  { text: 'Light Grey', value: '#c5c0b9' },
+  { text: 'White', value: '#c5c0b9' },
+])
+
+const lineHeight = ref('2')
+const lineHeights = ref([
+  { text: '1', value: '1' },
+  { text: '1.5', value: '1.5' },
+  { text: '2', value: '2' },
+  { text: '2.5', value: '2.5' },
+  { text: '3', value: '3' },
+])
+
+const letterSpacing = ref('1')
+const letterSpacings = ref([
+  { text: '1', value: '1' },
+  { text: '1.1', value: '1.1' },
+  { text: '1.2', value: '1.2' },
+  { text: '1.3', value: '1.3' },
+  { text: '1.4', value: '1.4' },
+  { text: '1.5', value: '1.5' },
+  { text: '1.6', value: '1.6' },
+  { text: '1.7', value: '1.7' },
+  { text: '1.8', value: '1.8' },
+  { text: '1.9', value: '1.9' },
+  { text: '2', value: '2' },
+])
+const backgroundColor = ref('#FFFDD0')
+const backgroundColors = ref([
+  { text: 'Beige', value: '#FFFDD0' },
+  { text: 'White', value: '#FFFFFF' },
+  { text: 'Peach', value: '#FFE5B4' },
+  { text: 'Dark Grey', value: '#26292a' },
+])
+</script>
+
+<template>
+  <!-- -->
+  <header class="header">
+    <h3>CSS styling for HTML output:</h3>
+    <span>
+      Affect current page?
+      <input type="checkbox" v-model="cssAffectPage" />
+    </span>
+    <div class="options">
+      Font size:
+      <select v-model="fontSize">
+        <option v-for="size in fontSizes" :value="size.value" :key="size.id">
+          {{ size.value }}
+        </option>
+      </select>
+      <span v-if="fontSize === 'Custom'">
+        <input v-model.lazy="fontSize" />
+      </span>
+
+      Font:
+      <select v-model="fontFamily">
+        <option v-for="family in fontFamilies" :value="family.value" :key="family.id">
+          {{ family.text }}
+        </option>
+      </select>
+
+      Font Color:
+      <select v-model="fontColor">
+        <option v-for="color in fontColors" :value="color.value" :key="color.id">
+          {{ color.text }}
+        </option>
+      </select>
+
+      Line Height:
+      <select v-model="lineHeight">
+        <option v-for="height in lineHeights" :value="height.value" :key="height.id">
+          {{ height.text }}
+        </option>
+      </select>
+
+      Letter Spacing:
+      <select v-model="letterSpacing">
+        <option v-for="spacing in letterSpacings" :value="spacing.value" :key="spacing.id">
+          {{ spacing.text }}
+        </option>
+      </select>
+
+      Background Color:
+      <select v-model="backgroundColor">
+        <option v-for="color in backgroundColors" :value="color.value" :key="color.id">
+          {{ color.text }}
+        </option>
+      </select>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  margin: '100px';
+}
+.options {
+  padding: '10px';
+}
+</style>
