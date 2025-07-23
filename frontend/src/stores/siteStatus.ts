0ef258b03ed287d5408c94d73b02aa4ce79368ee
KR9SIS <kristoferhelgi+github@protonmail.com>
2025-07-23 13:34:55 +0000

    Added a siteStatus Pinia store

---
+++ frontend/src/stores/siteStatus.ts
@@ -0,0 +1,22 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const siteStatus = defineStore('siteStatus', () => {
+  const markDown = ''
+  const fontSize = ref(localStorage.getItem('fontSize') || '12')
+  const fontFamily = ref(localStorage.getItem('fontFamily') || 'Lexend')
+  const fontColor = ref(localStorage.getItem('fontColor') || '#000000')
+  const lineHeight = ref(localStorage.getItem('lineHeight') || '2')
+  const letterSpacing = ref(localStorage.getItem('letterSpacing') || '0.1')
+  const backgroundColor = ref(localStorage.getItem('backgroundColor') || '#FFE5B4')
+
+  return {
+    markDown,
+    fontSize,
+    fontFamily,
+    fontColor,
+    lineHeight,
+    letterSpacing,
+    backgroundColor,
+  }
+})
